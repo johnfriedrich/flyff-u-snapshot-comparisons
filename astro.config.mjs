a257c3e817772baa79b6faa216949aa77aaddcc2
johnfriedrich <jonathanfriedrich@live.de>
2025-04-04 00:46:46 +0200

    Complete config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://johnfriedrich.github.io',
+    base: 'flyff-u-snapshot-comparisons',
 	integrations: [mdx(), sitemap()],
 });
