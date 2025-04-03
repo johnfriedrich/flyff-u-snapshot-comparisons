16f9da7de98fa57fc1d9d9a66a680f452c21402d
johnfriedrich <jonathanfriedrich@live.de>
2025-04-04 00:55:42 +0200

    Change Description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Snapshot Comparisons';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Flyff Universe Game Data Snapshot Comparisons. Stay up to date with (hidden) Game Updates';
