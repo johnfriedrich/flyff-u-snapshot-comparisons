7a226206acd8d7e897b7d7b1ffbf600f8aad5b1e
johnfriedrich <jonathanfriedrich@live.de>
2025-04-04 00:49:06 +0200

    Add slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://johnfriedrich.github.io',
-    base: 'flyff-u-snapshot-comparisons',
+    base: '/flyff-u-snapshot-comparisons',
 	integrations: [mdx(), sitemap()],
 });
