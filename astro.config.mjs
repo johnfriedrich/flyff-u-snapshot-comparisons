9ab131faaf48c5f0667e5e87d2737c217337892d
johnfriedrich <jonathanfriedrich@live.de>
2025-04-10 00:51:06 +0200

    Add sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import remarkTableOfContents from "./src/table-of-content-plugin.js";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://flyff-u-snapshot-comparisons.com',
-	integrations: [mdx(), sitemap()],
+  markdown: {
+    remarkPlugins: [remarkTableOfContents],
+  },
+  site: 'https://flyff-u-snapshot-comparisons.com',
+  integrations: [mdx(), sitemap()],
 });
